3d6f63ea771253e47e729357db1f6f24e354a9a2
MrDante <firstmrdante@gmail.com>
2019-04-23 11:58:44 +0700

    Init 6

---
+++ Backend6/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Backend6.Models;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Backend6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PostCategory> PostCategories { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<PostComment> PostComments { get; set; }
+
+        public DbSet<PostAttachment> PostAttachments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Post>().HasOne(x => x.Creator).WithMany().OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<PostComment>().HasOne(x => x.Creator).WithMany().OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
